01e2aa75e4f16fa1a902a8e236534d64d75010eb
dftvlnk <dftvlnk@gmail.com>
2013-07-09 11:31:50 +0200

    Organisation de la branche avec Compass pour le design.

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html id="placeprojet" lang="fr">
+    <head>
+        <!--CSS-->
+        <link rel="stylesheet" href="fonts/font.css" />
+        <link rel="stylesheet" href="base.css" />
+        <!--METADATA-->
+        <meta charset="UTF-8" />
+        <meta
+            name="description" 
+            content="PlaceProjet est site communautaire reliant tous ceux qui
+            recherche un partenaire ou de l'aide pour mener à bien les projets
+            de chacun."/>
+        <meta name="keywords" content="Gestion de Projet"/>
+    </head>
+
+    <body>
+        <header> <!--EN-TÊTE-->
+            <nav id="main_menu"><!--MENU-->
+                <h1 id="logo">placeProjet</h1><!--LOGO-->
+
+                <ul id="left_menu"><!--MENU GAUCHE-->
+                    <li>
+                        <a class="logo_nav" href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a class="projet_nav" href="projets.php">Explore projects</a>
+                    </li>
+                    <li>
+                        <a class="creation_nav" href="creer.php">Start your project</a>
+                    </li>
+                    <li>
+                        <a class="faq_nav" href="faq.php">FAQ</a>
+                    </li>
+                </ul>
+
+                <ul id=log_menu"><!--MENU DROIT-->
+                    <li>
+                        <a class="inscription_nav" href="inscription.php">Sign up</a>
+                    </li>
+                    <li>
+                        <a class="connexion_nav" href="connexion.php">Log in</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
+
+
